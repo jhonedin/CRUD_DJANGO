6959dcf5ecd97e6455389998ab28c217ecc98e55
jhonedin <jhonedin1888@hotmail.com>
2023-09-15 17:12:19 -0500

    Creamos una primer vista,url, y template.

--- Universidad/Aplicaciones/Academico/views.py
+++ Universidad/Aplicaciones/Academico/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Curso
 # Create your views here.
+def home(request):
+    cursos_listado = Curso.objects.all()
+    return render(request, "gestionCursos.html",{"cursos": cursos_listado})
