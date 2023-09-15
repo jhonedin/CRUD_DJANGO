3e207a3bdcb6d49eb330087e4dc5a04e8d895ed4
jhonedin <jhonedin1888@hotmail.com>
2023-09-15 14:18:05 -0500

    Se realiza la mgracion del modelo curso.

--- Universidad/Aplicaciones/Academico/models.py
+++ Universidad/Aplicaciones/Academico/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    codigo = models.CharField(primary_key=True,max_length=6)
+    codigo = models.CharField(primary_key=True,max_length=7)
     nombre = models.CharField(max_length=50)
     creditos = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto = "{0} - {1} - ({2})"
+        return texto.format(self.codigo, self.nombre, self.creditos)
+
+
