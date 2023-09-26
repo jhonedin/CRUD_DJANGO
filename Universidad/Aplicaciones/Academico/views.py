fd1d5f961112861d873daaf5591413537a36550c
jhonedin <jhonedin1888@hotmail.com>
2023-09-26 10:28:41 -0500

    Se implementa funcionalidad de mensajes.

--- Universidad/Aplicaciones/Academico/views.py
+++ Universidad/Aplicaciones/Academico/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Curso
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     cursos_listado = Curso.objects.all()
+    messages.success(request, '¡Cursos listados!')
     return render(request, "gestionCursos.html",{"cursos": cursos_listado})
 
 def registrarCurso(request):
@@ -11,6 +14,7 @@ def registrarCurso(request):
     creditos = request.POST['numCreditos']
 
     curso = Curso.objects.create(codigo=codigo,nombre=nombre,creditos=creditos)
+    messages.success(request, '¡Curso registrado!')
     return redirect('/')
 
 def edicionCurso(request,codigo):
@@ -26,12 +30,13 @@ def editarCurso(request):
     curso.nombre = nombre
     curso.creditos = creditos
     curso.save()
+    messages.success(request, '¡Curso actualizado!')
     return redirect('/')
 
 
 def eliminarCurso(request,codigo):
     curso = Curso.objects.get(codigo=codigo)
     curso.delete()
-
+    messages.success(request, '¡Curso eliminado!')
     return redirect('/')
 
